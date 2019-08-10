529d39c781aaada8e5b79d554c10c968b1b40495
mutsuki <mutsuki@momo86.net>
2019-08-10 21:01:39 +0900

    Add test code

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <mpi_local_rank.hpp>
+
+int main(int argc, char** argv) {
+	MPI_Init(&argc, &argv);
+
+	int rank, nprocs;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+	int local_rank = mtk::get_rank_within_node(MPI_COMM_WORLD);
+
+	std::printf("%4d / %4d : local rank = %4d\n", rank, nprocs, local_rank);
+
+	MPI_Finalize();
+}
