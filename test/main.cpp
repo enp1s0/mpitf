24e18ad43e3eecb633b3bd3ef61ba8a29712c749
mutsuki <mutsuki@momo86.net>
2019-09-30 20:37:38 +0900

    Add the test for `get_size_within_node`

--- test/main.cpp
+++ test/main.cpp
@@ -9,8 +9,9 @@ int main(int argc, char** argv) {
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
 	int local_rank = mtk::get_rank_within_node(MPI_COMM_WORLD);
+	int local_size = mtk::get_size_within_node(MPI_COMM_WORLD);
 
-	std::printf("%4d / %4d : local rank = %4d\n", rank, nprocs, local_rank);
+	std::printf("%4d / %4d : local rank = %4d / %4d\n", rank, nprocs, local_rank, local_size);
 
 	MPI_Finalize();
 }
