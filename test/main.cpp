904a5b0ba6a3ddbb2f7d558c5314016c97eb4978
mutsuki <mutsuki@momo86.net>
2020-05-26 15:02:22 +0900

    Update to use `mtk::get_comm_rank` and `mtk::get_comm_size`

--- test/main.cpp
+++ test/main.cpp
@@ -4,9 +4,8 @@
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 
-	int rank, nprocs;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+	int rank = mtk::get_comm_rank(MPI_COMM_WORLD);
+	int nprocs = mtk::get_comm_size(MPI_COMM_WORLD);
 
 	int local_rank = mtk::get_rank_within_node(MPI_COMM_WORLD);
 	int local_size = mtk::get_size_within_node(MPI_COMM_WORLD);
